d6fa3d0a00944e8cbf13ea6d7f1e7e7163772f40
XXLsmall <delieker61@gmail.com>
2018-06-22 14:41:09 +0200

    Upload

--- Agario-clone/blob.js
+++ Agario-clone/blob.js
@@ -1,14 +1,22 @@
+//Blobs scripts
+
+//Called on new creation of a Blob (setup)
+//x = xPos, y = yPos, r = ray
 function Blob(x, y, r) {
+	//Vector with coordinates
 	this.pos = createVector(x, y);
 	this.r = r;
 	
+	//Update default function
 	this.update = function() {
+		//Velocity Vector
 		var vel = createVector(mouseX-width/2, mouseY-height/2);
 		vel.sub(this.pos);
 		vel.setMag(3);
 		this.pos.add(vel);
 	}
 	
+	//On event eat
 	this.eats = function(other) {
 		var d = p5.Vector.dist(this.pos, other.pos);
 		if(d < this.r + other.r) {
@@ -21,8 +29,11 @@ function Blob(x, y, r) {
 			return false;
 		}
 	}
+	//Show default function
 	this.show = function() {
+		//Draw white
 		fill(255);
+		//Create the graphic Blob
 		ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
 	}
 }
